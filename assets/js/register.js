097e078dcd519510d9588fa2606d1040738c930a
Elmir9555 <elmiriu@code.edu.az>
2022-07-07 11:02:08 +0400

    end of login&RegisterPage

---
+++ assets/js/register.js
@@ -0,0 +1,43 @@
+//header
+$(document).ready(function(){
+    // language and currency dropdown
+    $(".language").click(function(){      
+        $(".dropdown-language").toggle(); 
+    });
+
+    $(".currency").click(function(){      
+        $(".dropdown-currency").toggle(); 
+    });
+    // language and currency dropdown
+
+    //account-dropwdown
+    $(".account").click(function(){
+        $(".dropdown-account").toggle();    
+    })
+    //account-dropwdown
+
+    //Notification dropdown
+    $(".notific").click(function(){
+        $(".dropdown-noti").toggle();   
+        if($(".dropdown-noti").css("display") == "block"){
+            $(".not-count").css("display","none"); 
+           } 
+        else if($(".dropdown-noti").css("display") =="none"){
+            $(".not-count").css("display","block"); 
+           }
+    })
+    //Notification dropdown
+
+    //waiting-basket
+    $(".waiting").click(function(){
+        $(".dropdown-waiting").toggle();    
+    })
+    //waiting-basket
+
+    //add-basket
+    $(".basket-card").click(function(){
+        $(".dropdown-basket").toggle();    
+    })
+     //add-basket
+});
+//header
